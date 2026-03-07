d9d34749af8c8b18b36ac47c0e5a835f655ccbd4
pk98723 <pavansaradhi73@gmail.com>
2026-03-08 02:04:47 +0530

    Updated AKS

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,74 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
+
+#################################
+# Log Analytics (AKS Monitoring)
+#################################
+
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = "demo-aks-logs"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+#################################
+# AKS Cluster
+#################################
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "demo-aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "demoaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  linux_profile {
+    admin_username = "azureuser"
+
+    ssh_key {
+      key_data = file("~/.ssh/id_rsa.pub")
+    }
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.log.id
+  }
+
+  tags = {
+    environment = "demo"
+  }
+}
+
+#################################
+# Allow AKS to Pull from ACR
+#################################
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
+
+#################################
+# Outputs
+#################################
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
